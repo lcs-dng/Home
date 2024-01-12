1ce0a808a985655003d6e06137f4c40f8fe6787b
Dexter Ng <DNg@lcs.on.ca>
2024-01-12 08:46:33 -0500

    Changed background to gradient.

--- Home/ContentView.swift
+++ Home/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            // Background
+            LinearGradient(
+                gradient: Gradient(colors: [Color.red, Color.blue]),
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+            .ignoresSafeArea()
+            
+            // Foreground
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+            
         }
-        .padding()
     }
 }
 
